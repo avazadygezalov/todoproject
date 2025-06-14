ce1112b15d0bdc768419044aaafc66f0116dc08f
avazadygezalov <avazadygezalov@icloud.com>
2025-06-14 17:23:00 +0700

    ready

--- action/sobitiye.go
+++ action/sobitiye.go
@@ -1,34 +1,31 @@
 package action
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"github.com/k0kubun/pp/v3"
 	"time"
 )
 
 type action struct {
+	name string
 	err  string
 	data string
 }
 
-var actions = make(map[string]action)
+var actions = make([]action, 0)
 
 func ActT(a string) {
-	hash := md5.Sum([]byte(time.Now().Format("2006-01-02 15:04:05"))) // Сохраняем массив
-	b := hex.EncodeToString(hash[:])
-	actions[a+" | "+b] = action{
+	actions = append(actions, action{
+		name: a,
 		data: time.Now().Format("2006-01-02 15:04:05"),
-	}
+	})
 }
 
 func ActF(a string, b string) {
-	hash := md5.Sum([]byte(time.Now().Format("2006-01-02 15:04:05"))) // Сохраняем массив
-	c := hex.EncodeToString(hash[:])
-	actions[a+" | "+c] = action{
+	actions = append(actions, action{
+		name: a,
 		err:  b,
 		data: time.Now().Format("2006-01-02 15:04:05"),
-	}
+	})
 }
 
 func GetAction() {
